ae17ef98ebc5a2eea15118ee3615f7f7748c2d3d
marlinism <76256179+marlinism@users.noreply.github.com>
2022-05-23 01:19:31 -0700

    Design and Do some Initial Code

---
+++ GroceryInventoryApp/GroceryInventoryApp/Controllers/FrontPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FrontPageViewController.swift
+//  GroceryInventoryApp
+//
+//  Created by 🌟 marlin 🌟 on 5/21/22.
+//
+
+import UIKit
+
+class FrontPageViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var inventoryButton: UIButton!
+    @IBOutlet weak var scanButton: UIButton!
+    
+}
